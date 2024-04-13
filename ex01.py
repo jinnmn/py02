0a5335e684984b7d922f784100a282f143c8faab
123 <123>
2024-04-13 23:59:41 +0300

    накидали архитектуру

---
+++ ex01.py
@@ -0,0 +1,27 @@
+#def menu?
+
+def write_contact( contact ):
+    """Функция принимает словарь и записывает его в файл
+    data.txt"""
+    return NULL
+
+def read_line( line ):
+    """Функция принимает контакт от пользователя
+    и формирует его в словарь"""
+    return NULL
+
+def read_data( file ):
+    """Функция читает принятый файл и возвращает
+    контакты списком словарей
+    """
+    return NULL
+
+def print_data( data ):
+    """Ф-я принимает список словарей контактов и выодит
+    в консоль"""
+    return NULL
+
+def find_data( income_data, key ):
+    """Ф-я принимает базу контактов и изменят её
+    поисковым запросом"""
+    return NULL
